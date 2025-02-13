e5c17827a97a876c0e98abe03513db49017f1ea4
younghwan314 <younghwan314@naver.com>
2025-02-13 10:29:24 +0900

    init commit

---
+++ src/test/java/com/example/layered/LayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.layered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
